01b4dc558c2e263f375292d272d646ce4e6093b6
u37424 <lucaspirka@gmail.com>
2022-11-14 15:08:52 +0100

    Da, ohne farben :(

--- src/main/java/de/medieninformatik/Message.java
+++ src/main/java/de/medieninformatik/Message.java
@@ -1,16 +1,13 @@
 package de.medieninformatik;
 
-import javafx.scene.paint.Color;
 
 import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Random;
 
 public class Message implements Serializable {
     private String text;
     private String alias;
-    private Color colorUni;
     private InetAddress sender;
 
     final static long serialVersionUID = 123456789L;
@@ -22,16 +19,6 @@ public class Message implements Serializable {
             this.alias = InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException e) {
             e.printStackTrace();
-        }
-        Random r = new Random();
-        int n = r.nextInt(1,6);
-        switch (n){
-            case 1: this.colorUni = Color.RED;
-            case 2: this.colorUni = Color.RED;
-            case 3: this.colorUni = Color.RED;
-            case 4: this.colorUni = Color.RED;
-            case 5: this.colorUni = Color.RED;
-            case 6: this.colorUni = Color.RED;
         }
     }
 
@@ -52,7 +39,4 @@ public class Message implements Serializable {
         return sender;
     }
 
-    public Color getColorUni() {
-        return colorUni;
-    }
 }
